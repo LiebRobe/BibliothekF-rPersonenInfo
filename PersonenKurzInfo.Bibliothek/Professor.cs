a7fde63b01123b532452bd6c9d4e668482233688
Liebhaber Robert <liebhaber.robert@gmail.com>
2019-05-28 20:14:13 +0200

    Klasse Professor erstellt

---
+++ PersonenKurzInfo.Bibliothek/Professor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static System.Console;
+
+namespace PersonenKurzInfo.Bibliothek
+{
+    /*    Klasse Professor mit
+       *       - der automatisch implementierten Eigenschaften Fach (string)
+       *       - einem Konstruktor, der Werte für Vorname, Nachname, Alter und Fach
+       *         übernehmen kann.
+       *       - Diese Klasse muss die abstrakte Methode AusgebenKurzInfo implementieren,
+       *         sodass die Methode z.B. folgende Ausgabe macht:
+       *            Heinz Wagner (52), Professor für Informatik
+       *         Dabei soll die ToString-Methode der Basisklasse verwendet werden.
+       */
+    public class Professor : Person
+    {
+        public string Fach { get; set; }
+        public Professor(string vorname, string nachname, int alter, string fach) : base(vorname, nachname, alter) => (Fach) = (fach);
+        public override void KurzInfoAusgeben()
+        {
+            Write(base.ToString() + string.Format($"{nameof(Professor)} für {Fach}"));
+        }
+    }
+}
