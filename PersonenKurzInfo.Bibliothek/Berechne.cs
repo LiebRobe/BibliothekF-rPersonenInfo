14ad46af6d0c7d31af5c5e31db30dc25b004546c
Liebhaber Robert <liebhaber.robert@gmail.com>
2019-05-28 20:36:50 +0200

    GeburtsjahrBerechnen in Geburtsjahr umbenannt

--- PersonenKurzInfo.Bibliothek/Berechne.cs
+++ PersonenKurzInfo.Bibliothek/Berechne.cs
@@ -6,7 +6,7 @@ namespace PersonenKurzInfo.Bibliothek
 {
     public static class Berechne
     {
-        public static int GeburtsjahrBerechnen(Person p)
+        public static int Geburtsjahr(Person p)
         {
             int aktuellesJahr = DateTime.Now.Year;
             return aktuellesJahr - p.Alter;
